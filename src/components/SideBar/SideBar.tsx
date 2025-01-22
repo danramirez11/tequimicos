f05493b063460c3d59e96b792e87336a1720e878
Dan <daramirezp1112@gmail.com>
2025-01-21 21:09:50 -0500

    feat: add sidebar and styles

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,10 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import './SideBar.css';
+import { FaCashRegister, FaHistory } from 'react-icons/fa';
 
 const SideBar = () => {
     return (
         <section className="sidebar">
-            <h2>Sidebar</h2>
-            <p>This is a sidebar</p>
+            <h3>Todo envases y químicos</h3>
+
+            <div className='sidebar-links'>
+
+            <NavLink to='/facturacion' className={({ isActive, isPending }) => isPending ? 'link pending' : isActive ? 'link active' : 'link'}>
+                <FaCashRegister/> Pedidos
+            </NavLink>
+
+            <NavLink to='/historial' className={({ isActive, isPending }) => isPending ? 'link pending' : isActive ? 'link active' : 'link'}>
+                <FaHistory/> Historial
+            </NavLink>
+
+            </div>
+            
         </section>
     );
 }
