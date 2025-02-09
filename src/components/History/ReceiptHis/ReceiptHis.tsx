b38ed42273b9f0a05e1940b25f60ff20fce7e650
Dan <daramirezp1112@gmail.com>
2025-02-09 14:01:23 -0500

    feat(history): show receipt details

--- src/components/History/ReceiptHis/ReceiptHis.tsx
+++ src/components/History/ReceiptHis/ReceiptHis.tsx
@@ -3,6 +3,7 @@ import { Receipt } from "../../../types/products";
 import './ReceiptHis.css'
 import { formatDate } from "../../../utils/functions";
 import { FaPrint } from "react-icons/fa6"; 
+import { FaTrash } from "react-icons/fa";
 
 type ReceiptHisProps = {
     receipt: Receipt;
@@ -11,7 +12,7 @@ type ReceiptHisProps = {
 const ReceiptHis = ({receipt}: ReceiptHisProps) => {
     const [ isActive, setIsActive ] = useState<boolean>(false);
 
-    const easilyReadDate = formatDate(receipt.date);
+    const easilyReadDate = formatDate(receipt.date, receipt.timestamp);
     const productNames = receipt.products.map(p => p.name).join(', ');
 
     return (
@@ -21,13 +22,90 @@ const ReceiptHis = ({receipt}: ReceiptHisProps) => {
             {
                 isActive ?
 
-                <div>hold on</div> :
+                <div className="receipt-his-info">
+                    <div className="receipt-his-products">
+                        { receipt.products.map(p => {
+                            if (p.type === 'container') {
+                                return (
+                                    <>
+                                    <p key={p.id}><b>{p.quantity}</b> {p.name}</p>
+                                    <div className="indent">
+                                        {p.lids.map(l => {
+                                            return (
+                                                <>
+                                                <p key={l.id}><b>{l.quantity}</b> {l.name}</p>
+                                                <p>{l.spout ? l.spout === 'eco' ? 'Anterior' : 'Laboratorio' : ''}</p>
+                                                <div className="indent">
+                                                    {l.colors.map(c => {
+                                                        return (
+                                                            <p key={c.name}>{c.quantity} {c.name}</p>
+                                                        )
+                                                    })}
+                                                </div>
+                                                {l.lazo ? 
+                                                    l.lazo.map(lazo => {
+                                                        return (
+                                                            <div className="indent" key={lazo.name}>
+                                                                <p>Lazos:</p>
+                                                                <p>{lazo.quantity} {lazo.name}</p>
+                                                            </div>
+                                                        )
+                                                    }) : ''
+                                                }
+                                                </>
+                                            )
+                                        })}
+                                    </div>
+                                    <p className="price">${p.price}</p>
+                                    </>
+                                )
+                            } else if (p.type === 'lid') {
+                                return (
+                                    <>
+                                    <p key={p.id}><b>{p.quantity}</b> {p.name}</p>
+                                    <div className="indent">
+                                        {p.colors.map(c => {
+                                            return (
+                                                <p key={c.name}>{c.quantity} {c.name}</p>
+                                            )
+                                        })}
+                                    </div>
+                                    <p className="price">${p.price}</p>
+                                    </>
+                                )
+                            } else if (p.type === 'containerOnly'){
+                                return (
+                                    <>
+                                    <p key={p.id}><b>{p.quantity}</b> {p.name}</p>
+                                    <p className="price">${p.price}</p>
+                                    </>
+                                )
+                            } else if (p.type === 'misc'){
+                                return (
+                                    <>
+                                    <p key={p.id}><b>{p.quantity}</b> {p.name}</p>
+                                    <p className="price">${p.price}</p>
+                                    </>
+                                )
+                            }
+                        })}
+                    </div>
+                    <p>{easilyReadDate} <br/> {receipt.payment}</p>
+                    <p>{receipt.isDelivery ? 'Domicilio' : 'Punto de venta'}</p>
+                    <p><b>${receipt.total}</b></p>
+                    <div className="receipt-his-buttons">
+                    <button className="blue"><FaPrint/></button>
+                    <button className="red"><FaTrash/></button>
+                </div>
+                </div> 
+                
+                :
 
-                <div className="receipt-his-info flex">
+                <div className="receipt-his-info">
                     <p className="receipt-his-products">
                         { productNames }
                     </p>
-                    <p>{easilyReadDate}, {receipt.hour}</p>
+                    <p>{easilyReadDate}</p>
                     <p>{receipt.isDelivery ? 'Domicilio' : 'Punto de venta'}</p>
                     <p><b>${receipt.total}</b></p>
                     <button className="blue"><FaPrint/></button>
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,25 @@
-export const formatDate = (dateStr: string) => {
+import { Timestamp } from "firebase/firestore";
+
+export const formatDate = (dateStr: string, timestamp: Timestamp | undefined) => {
     const [day, month] = dateStr.split('/').map(Number);
 
     const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
 
-    return `${months[month - 1]} ${day}`;
+    let readibleHour;
+
+    if ( timestamp ) {
+        const date = timestamp.toDate();
+        let hour = date.getHours() % 12;
+        hour = hour ? hour : 12;
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const ampm = date.getHours() >= 12 ? 'pm' : 'am';
+
+        readibleHour = `${hour}:${minutes} ${ampm}`;
+    } else {
+        readibleHour = '00:00 am';
+    }
+
+    
+
+    return `${months[month - 1]} ${day}, ${readibleHour}`;
 }
